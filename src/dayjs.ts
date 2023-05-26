14a9e7e9f3bf0554b4dbc9c166c8927ecdef3f7f
ilya-tishonok <ilya.tishonok@satelliteinnovations.io>
2023-05-26 12:22:47 +0300

    ND-3560 Add custom parser plugin to dayjs instance

--- src/dayjs.ts
+++ src/dayjs.ts
@@ -3,10 +3,12 @@ import isLeapYear from 'dayjs/plugin/isLeapYear';
 import weekOfYear from 'dayjs/plugin/weekOfYear';
 import objectSupport from 'dayjs/plugin/objectSupport';
 import isoWeeksInYear from 'dayjs/plugin/isoWeeksInYear';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
 
 dayjs.extend(isLeapYear);
 dayjs.extend(weekOfYear);
 dayjs.extend(objectSupport);
 dayjs.extend(isoWeeksInYear);
+dayjs.extend(customParseFormat);
 
 export default dayjs;
